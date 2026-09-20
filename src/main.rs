2a11d3c9bbb7688030cc0c6a1ccfb753e746d254
Vinícius Freitas <vinifreitas.d.a@gmail.com>
2026-09-20 11:00:45 -0300

    fill template

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(phase_unwrapping_visualizer::TemplateApp::new(cc)))),
     )
 }
 
@@ -52,7 +52,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(phase_unwrapping_visualizer::TemplateApp::new(cc)))),
             )
             .await;
 
